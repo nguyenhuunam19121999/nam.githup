21a613d114929faa7bfe5dd6bbfe534589efa2f1
Your Name <your_email@example.com>
2026-07-16 18:49:26 +0900

    debug: add logging to firebase podfile fix plugin

--- plugins/withFirebaseModularHeadersFix.js
+++ plugins/withFirebaseModularHeadersFix.js
@@ -9,13 +9,14 @@ module.exports = function withFirebaseModularHeadersFix(config) {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let contents = fs.readFileSync(podfilePath, 'utf-8');
 
-      // 1. Bắt buộc: báo cho React Native Firebase biết đang dùng static frameworks
-      // Phải đặt ở đầu file, TRƯỚC use_frameworks!
+      console.log('[withFirebaseModularHeadersFix] Podfile path:', podfilePath);
+      console.log('[withFirebaseModularHeadersFix] already has $RNFirebaseAsStaticFramework?', contents.includes('$RNFirebaseAsStaticFramework'));
+
       if (!contents.includes('$RNFirebaseAsStaticFramework')) {
         contents = `$RNFirebaseAsStaticFramework = true\n${contents}`;
+        console.log('[withFirebaseModularHeadersFix] INSERTED $RNFirebaseAsStaticFramework');
       }
 
-      // 2. Cho phép non-modular includes trong framework modules
       const snippet = `
     installer.pods_project.targets.each do |target|
       target.build_configurations.each do |bc|
@@ -24,14 +25,18 @@ module.exports = function withFirebaseModularHeadersFix(config) {
     end
 `;
 
+      console.log('[withFirebaseModularHeadersFix] already has CLANG_ALLOW_NON_MODULAR?', contents.includes('CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES'));
+
       if (!contents.includes('CLANG_ALLOW_NON_MODULAR_INCLUDES_IN_FRAMEWORK_MODULES')) {
         contents = contents.replace(
           /post_install do \|installer\|/,
           `post_install do |installer|\n${snippet}`
         );
+        console.log('[withFirebaseModularHeadersFix] INSERTED CLANG_ALLOW_NON_MODULAR snippet');
       }
 
       fs.writeFileSync(podfilePath, contents);
+      console.log('[withFirebaseModularHeadersFix] Podfile written successfully');
       return config;
     },
   ]);
